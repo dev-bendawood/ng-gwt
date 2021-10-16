51ad3ad2899b0702942d2cb29c75a3a3c9919787
dev-bendawood <dev.bendawood@gmail.com>
2021-10-16 02:12:26 -0700

    20211016

---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.component.html',
+  styles: [``]
+})
+export class CustomersComponent implements OnInit  {
+  customers: any;
+ 
+  constructor(private http: HttpClient) { }
+ 
+  ngOnInit() {
+    this.http.get("https://localhost:5001/api/customers")
+    .subscribe(response => {
+      this.customers = response;
+    }, err => {
+      console.log(err)
+    });
+  }
+}
